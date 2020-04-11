cc292d32790684a83052b225f1a185b484b70c39
Alex S Grimes <alex@alexgrimes.com>
2020-04-11 22:42:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,8 @@ end
 def using_pop(array)
  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   array = continents.pop
-  p array 
+  p array
+  p continents.size
 end 
 
 def pop_with_args(array)
